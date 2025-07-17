9c0248f9f2429377943f2c129ee3bb8cabe830de
ShrimpOnigiri <kiko.zyx@gmail.com>
2025-07-17 12:34:56 +0100

    Modify products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SuperShop.Data.Entities
 {
@@ -8,6 +9,8 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} cannot contain more than {1} characters in length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
@@ -17,11 +20,11 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display (Name = "Last Purchase")]
-        public DateTime LastPurchase {  get; set; }
+        public DateTime? LastPurchase {  get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
