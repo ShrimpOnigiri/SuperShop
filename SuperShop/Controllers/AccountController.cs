3c3c2815aaeec84cc33afd0deb7d306bd538076b
ShrimpOnigiri <kiko.zyx@gmail.com>
2025-09-14 16:34:51 +0100

    Change user profile and password

--- SuperShop/Controllers/AccountController.cs
+++ SuperShop/Controllers/AccountController.cs
@@ -47,7 +47,7 @@ namespace SuperShop.Controllers
 
         public async Task<IActionResult> Logout()
         {
-        await _userHelper.LogoutAsync();
+            await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
 
@@ -83,7 +83,7 @@ namespace SuperShop.Controllers
                         Password = model.Password,
                         RememberMe = false,
                         Username = model.Username
-                    };  
+                    };
 
                     var result2 = await _userHelper.LoginAsync(loginViewModel);
                     if (result2.Succeeded)
@@ -92,9 +92,78 @@ namespace SuperShop.Controllers
                     }
 
                     ModelState.AddModelError(string.Empty, "The user couldn't be logged in.");
-                    
+
                 }
-                
+
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ChangeUser()
+        {
+            var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+            var model = new ChangeUserViewModel();
+            if (user != null)
+            {
+                model.FirstName = user.FirstName;
+                model.LastName = user.LastName;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeUser(ChangeUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    var response = await _userHelper.UpdateUserAsync(user);
+                    if (response.Succeeded)
+                    {
+                        ViewBag.UserMessage = "User updated!";
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, response.Errors.FirstOrDefault().Description);
+                    }
+                }
+            }
+            return View(model);
+
+
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return this.RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User no found.");
+                }
             }
             return View(model);
         }
